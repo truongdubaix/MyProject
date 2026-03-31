dcefe373451da02de397188dd00c330635714ad7
truongdubaix <truongcss1220@gmail.com>
2026-03-31 22:42:26 +0700

    remove middlewares

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
-import { getAllNews, getNewsById, createNews, updateNews, deleteNews } from "../controllers/newsController.js";
-import upload, { handleUploadResponse } from "../middlewares/upload.js";
-// import verifyToken from "../middlewares/authMiddleware.js";
-// NÊU BẠN CÓ ROLE MIDDLEWARE THÌ DÙNG, TẠM THỜI MÌNH KHÔNG KIỂM TRA QUYỀN TRÊN ROUTE ĐỂ CODE NHANH,
-// NHƯNG PUBLIC API THÌ KHÔNG GET ĐƯỢC VẤN ĐỀ
+import {
+  getAllNews,
+  getNewsById,
+  createNews,
+  updateNews,
+  deleteNews,
+} from "../controllers/newsController.js";
+import upload, { handleUploadResponse } from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get("/", getAllNews);
 router.get("/:id", getNewsById);
-router.post("/", upload.single("imageFile"), createNews); 
+router.post("/", upload.single("imageFile"), createNews);
 router.put("/:id", upload.single("imageFile"), updateNews);
 router.delete("/:id", deleteNews);
 
